dbf139aab339171f779e62468b3fbdfdd73ea14c
surajNirala <suraj.nirala1995@gmail.com>
2024-04-15 18:47:56 +0530

    code update1

--- go.mod
+++ go.mod
@@ -4,10 +4,14 @@ go 1.22.0
 
 require github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 
-require go4.org v0.0.0-20230225012048-214862532bf5 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	go4.org v0.0.0-20230225012048-214862532bf5 // indirect
+)
 
 require (
 	github.com/bradfitz/slice v0.0.0-20180809154707-2b758aa73013
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/google/go-cmp v0.6.0
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,122 +1,94 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"sort"
-	"strconv"
 
-	"github.com/gorilla/mux"
-	"github.com/surajNirala/web/handlers"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func add(a, b int) int {
-	return a + b
-}
-
-func sub(a, b int) int {
-	return a - b
-}
-
-func airth(data string) func(int, int) int {
-	// fmt.Println("type :", data)
-	if data == "add" {
-		return add
-	} else {
-		return sub
-	}
-}
-
-func arrSort() map[string]string {
-	var arr = make(map[string]string)
-	arr["1"] = "test1"
-	arr["2"] = "test2"
-	arr["5"] = "test5"
-	arr["10"] = "test10"
-	arr["9"] = "test9"
-	arr["3"] = "test3"
-	arr["4"] = "test4"
-	return arr
-	// fmt.Println("Before Sorting: ", arr)
-}
-
-func ascOrder(arrData []int) {
-	sort.Slice(arrData, func(i, j int) bool {
-		return arrData[i] < arrData[j]
-	})
-	fmt.Println("Sorted sortKey (ascending):", arrData)
-}
-
-func descOrder(arrData []int) {
-	sort.Slice(arrData, func(i int, j int) bool {
-		return arrData[i] > arrData[j]
-	})
-	fmt.Println("Sorted sortKey (descending):", arrData)
-}
-
-func getKeyFromArr(arrData map[string]string) []int {
-	// keys := []int{}
-	keys := make([]int, 0, len(arrData))
-	for key, _ := range arrData {
-		intKey, err := strconv.Atoi(key)
-		if err != nil {
-			panic(err) // Handle error if conversion fails
-		}
-		keys = append(keys, intKey)
-	}
-	return keys
-}
-
-func getValue() {
-	data := make(map[string]string)
-	data["1"] = "s1"
-	data["2"] = "s2"
-	data["4"] = "s4"
-	data["3"] = "s3"
-	fmt.Println("data", data)
-	key := make([]string, 0)
-	for index, _ := range data {
-		key = append(key, index)
-	}
-	fmt.Println("key := ", key)
-}
-
-type constant string
-type unconstant string
-
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to Home Page!")
-	fmt.Println("Welcome to Home Page!")
+type User struct {
+	ID    int    `json:"id"`
+	Phone string `json:"phone"`
+	Email string `json:"email"`
+	// Add other fields as needed
 }
 
 func main() {
+	fmt.Println("deded")
+	http.HandleFunc("/health", healthCheckHandler)
+	http.HandleFunc("/get-users", getUsers)
+	fmt.Println("Server is running on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
 
-	router := mux.NewRouter()
-	// Define your API routes
-	router.HandleFunc("/", HomeHandler).Methods("GET")
-	router.HandleFunc("/get-user", handlers.GetUser).Methods("GET")
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	// Replace the connection string with your MySQL connection string
+	db, err := sql.Open("mysql", "srj:12345678@tcp(localhost:3306)/go_crud1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-	// Start the HTTP server
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// Check if the database connection is successful
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
 
-	// getValue()
-	// var a constant = "10"
-	// a = "20"
-	// var a1 unconstant = "srj"
-	// var a1 unconstant = "srjss"
-	// fmt.Println("a => ", a)
-	// fmt.Println("a1 => ", a1)
-	// arrData := arrSort()
-	// sortKey := getKeyFromArr(arrData)
-	// fmt.Println("Key-Value Pair", arrData)
-	// fmt.Println("Keys", sortKey)
-	// sort.Strings(sortKey)
-	// fmt.Println("Sorted-Keys", sortKey)
-	// ascOrder(sortKey)
-	// fmt.Println("ss")
+	rows, err := db.Query("SELECT id, phone, email FROM users limit 20")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
 
-	// res := airth("add")
-	// fmt.Printf("TYpe %T", res)
-	// fmt.Println("Res :=", res(2, 1))
+	var users []User
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.ID, &user.Phone, &user.Email); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Convert users slice to JSON
+	jsonBytes, err := json.Marshal(users)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Set content type header and write JSON response
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonBytes)
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	// Replace the connection string with your MySQL connection string
+	// db, err := sql.Open("mysql", "srj:12345678@tcp(localhost:3306)/go_crud1")
+	db, err := sql.Open("mysql", "srj:admin@tcp(65.2.37.239:3306)/srj_crud")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error connecting to database: %v", err)
+		return
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error pinging database: %v", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Database connection successful")
 }
